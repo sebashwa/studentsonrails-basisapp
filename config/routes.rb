b5571f038adc38f5c4bacdbd28e3d1f107fb1c09
Marcus Jenz <marcus@MrcsSlpN.(none)>
2013-11-09 16:18:11 +0100

    Artist added. Can be created, listed, edited and deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Basisapp::Application.routes.draw do
   devise_for :users
   resources :posts
+  resources :artists
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
