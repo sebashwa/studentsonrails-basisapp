f22e8d5edf8b2e9b904d85a846e12d1c6e46480c
Ole Reifschneider <mail@ole-reifschneider.de>
2013-11-09 11:42:28 +0100

    add some feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Basisapp::Application.routes.draw do
+  devise_for :users
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +42,7 @@ Basisapp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
