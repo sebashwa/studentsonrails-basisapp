557d8f58b32c33bf3c4fac622fdae5f9723305a3
sssggr <n.siessegger@zeromail.org>
2013-11-09 15:19:38 +0100

    Added Name to User / Added Timestamp and Author to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109103441) do
+ActiveRecord::Schema.define(version: 20131109133416) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20131109103441) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
