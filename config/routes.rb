2623a72a7f436b2cc765b6d1147fdeb806a193e8
Andi <andi.helms90@gmail.com>
2013-11-09 17:53:00 +0100

    Add comments to posts, Set default username

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Basisapp::Application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments, except: [:show,:edit,:update,:index]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
