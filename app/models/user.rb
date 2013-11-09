fbbc6b720bb757ab4061417f483cfad6e052453c
Marcus Jenz <marcus@MrcsSlpN.(none)>
2013-11-09 16:38:43 +0100

    Now really no relation between user and song

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :posts
-  has_many :songs
+  has_many :posts  
 end
